72d0c1529a64eb3be82c6501df7dcbf49acd6157
martensonbj <martensonbj@gmail.com>
2016-09-15 12:57:20 -0600

    One test passing

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,8 +1,9 @@
-require('babel-register')
+require('babel-register')({
+  presets: ["react", "es2015"]
+});
 require('babel-polyfill')
 
-// import { expect } from 'chai'
-// import sinon from 'sinon'
+
 
 global.document = require('jsdom').jsdom(
    "<head><meta charset='UTF-8'><title>Shoot The Breeze</title></head><body><div id='application'></div></body>"
@@ -10,6 +11,6 @@ global.document = require('jsdom').jsdom(
 
 global.window = document.defaultView
 global.navigator = window.navigator
-//
+
 // global.expect = expect
 // global.sinon = sinon
