4fd5d2251293061f78508521963467f2ed0bf159
martensonbj <martensonbj@gmail.com>
2016-09-15 12:08:03 -0600

    Add a few config files for firebase

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow, mount, render } from 'enzyme'
-import App from '../lib/components/App'
+import App from 'lib/components/App'
 import { expect } from 'chai'
 
 
