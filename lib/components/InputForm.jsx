b12c2012713b791ce0c6615ce4c49b676e771ba2
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-19 17:31:48 -0600

    Submit button working upon refactor

--- lib/components/InputForm.jsx
+++ lib/components/InputForm.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import firebase, { reference, signIn } from '../firebase';
 import { pick, map, extend } from 'lodash';
 
-const InputForm = ({draftedMessage, onDraftedMessageChange})=>{
-  return (<div>
-    <input
-      placeholder="Message…"
-      value={draftedMessage}
-      onChange={onDraftedMessageChange}
-    />
+const InputForm = ({draftedMessage, onDraftedMessageChange, onMessageSubmit})=>{
+  return (
+    <div>
+      <input
+        placeholder="Message…"
+        value={draftedMessage}
+        onChange={onDraftedMessageChange}
+      />
+      <button onClick={onMessageSubmit}>Add New Message</button>
+
     </div>
   );
 }
