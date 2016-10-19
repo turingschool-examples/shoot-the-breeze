dcabc190ec78f19f95afae7273506b157ed72aed
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-19 15:51:23 -0600

    Refactoring RenderMessages previous code

--- lib/components/RenderMessages.jsx
+++ lib/components/RenderMessages.jsx
@@ -1,3 +1,16 @@
 import React, { Component } from 'react'
 import firebase, { reference, signIn } from '../firebase';
 import { pick, map, extend } from 'lodash';
+
+const RenderMessages = ({messages})=>{
+    return(
+    <ul>
+      { messages.map(m =>{
+        return <li key={m.key}>{m.user.displayName}: {m.content}</li>
+        })
+      }
+    </ul>
+    );
+}
+
+module.exports = RenderMessages;
