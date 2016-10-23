ca958405dda9dc2b940c1621a2b2d4699de07fe4
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-23 12:11:37 -0600

    Attempting to make some tests

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -5,12 +5,24 @@ import { assert, expect } from 'chai';
 import moment from 'moment';
 
 import Application from '../lib/components/Application';
+import InputForm from '../lib/components/InputForm';
+import FilterMessages from '../lib/components/FilterMessages';
+import DisplayMessages from '../lib/components/DisplayMessages';
 
 describe('Application', () => {
-
-  it('renders as a <div>', () => {
+  it('renders as a <section>', () => {
     const wrapper = shallow(<Application />)
-    assert.equal(wrapper.type(), 'div');
+    assert.equal(wrapper.type(), 'section');
   });
+
+  it('renders a message on the page on click of submit button',() =>{
+    const wrapper = mount(<InputForm />);
+    const input = wrapper.find('#message-input');
+
+    input.simulate('change', {targer: {value: 'hello world'}});
+    wrapper.find('.submit-btn').simulate('click');
+    expect(wrapper.text().to.equal('hello world'));
+  });
+
 
 });
