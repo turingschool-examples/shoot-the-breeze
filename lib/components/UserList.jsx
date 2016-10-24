4e5b9e8966f47e39094356e03ac89f8dd37eb89b
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-24 15:52:42 -0600

    I dont know

--- lib/components/UserList.jsx
+++ lib/components/UserList.jsx
@@ -5,22 +5,21 @@ import { sortedUniqBy } from 'lodash';
 export default class UserList extends Component {
   get displayUsers() {
     let users = this.props.messages.map(m => {
-      return {userName: m.user.displayName, email: m.user.email};
+      return { userName: m.user.displayName, email: m.user.email};
     });
     return sortedUniqBy(users, 'userName');
   }
 
   render() {
     return (
-      <aside id="user-list">
+      <div id="user-list">
         <h3>Users</h3>
         <ul>
           {this.displayUsers.map(u => {
-            return
-            <li key= {u.userName}>{u.userName} ({u.email})</li>
+            return <li key= {u.createdAt}>{u.userName} ({u.email})</li>
           })}
         </ul>
-      </aside>
+      </div>
     )
   }
 }
