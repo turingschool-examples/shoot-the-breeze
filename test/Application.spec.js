b4dacb63d226ee9f861a9bb35fbac30003a27b49
KINAN WHYTE <kinanwhyte@gmail.com>
2016-10-24 14:17:54 -0600

    adds users to the userlist on app render

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -3,9 +3,10 @@ import React from 'react'
 import { shallow, mount, render } from 'enzyme'
 import { assert } from 'chai'
 
+let sinon = require('sinon')
+
 import Application from '../lib/components/Application'
 import MessageInput from '../lib/components/MessageInput'
-
 
 
 describe('Unit Test | Application', () => {
@@ -16,12 +17,22 @@ describe('Unit Test | Application', () => {
     const wrapper = shallow(<Application />)
     assert.equal(wrapper.type(), 'div')
   })
+
+  // it('should render the org name', () => {
+  //   const wrapper = shallow(<Application />)
+  //   assert.equal(wrapper.contains('turingschool')).to.be.true
+  // })
 })
 
 describe('Unit Test | MessageInput', () => {
   it('can mount with no properties', () => {
     const wrapper = shallow(<MessageInput />)
   })
+  // it('calls componentDidMount', () => {
+  //   sinon.spy(MessageInput.prototype, 'componentDidMount')
+  //   const wrapper = mount(<MessageInput />)
+  //   expect(MessageInput.prototype.componentDidMount.calledOnce).to.equal(true)
+  // })
   it('renders a "submit-button"', () => {
     const wrapper = mount(<MessageInput />)
     assert(wrapper.find('.submit-button'))
@@ -34,8 +45,14 @@ describe('Unit Test | MessageInput', () => {
     const wrapper = shallow(<MessageInput />)
     assert.equal(wrapper.type(), 'form')
   })
+// ________
+  // it('simulates click events', () => {
+  //   const onButtonClick = sinon.spy()
+  //   const wrapper = shallow(<MessageInput />)
+  // })
+  //
+  //
 })
-
 
 describe('Feature Test | MessageInput', () => {
   it('should change the "draftMessage" state with new user input', () => {
@@ -58,3 +75,41 @@ describe('Feature Test | MessageInput', () => {
     }, 1)
   })
 })
+
+
+
+// console.log(wrapper.debug())
+
+//
+// describe('app.jsx renders the likes counter',function(){
+//   it('should render the application',function(){
+//
+//     const wrapper = shallow(<App/>)
+//
+//     expect(wrapper.contains(<LikesCounter initialCount={0} />)).to.be.true
+//     expect(wrapper.props()).to.deep.equal({ initialCount: 0 })
+//   })
+//
+//
+//   it('should have the button text rendered onto the page', function(){
+//     const wrapper = render(<App/>)
+//     expect(wrapper.text()).to.contain('Likes: 0Like! (+1)Dislike! (-1)')
+//   })
+// })
+
+
+
+// describe('likes counter',function(){
+//   it('should have 2 action button props', function(){
+//     const wrapper = render(<LikesCounter/>)
+//     expect(wrapper.find('.ActionButton')).to.have.length(2)
+//   })
+//
+//   it('should allow me to click the action button', function(){
+//     const wrapper = mount(<LikesCounter/>)
+//     wrapper.state().count = 0 // we must do this because this value isn't set initially
+//     var button = wrapper.find('#like').simulate('click')
+//
+//     expect(wrapper.state().count).to.equal(1)
+//   })
+// })
