6e2531c54f9ee60d984e46fce758e73baef7266a
Hilary Lewis <hilarylewis92@gmail.com>
2016-10-25 13:17:02 -0600

    Commit to push to branch

--- lib/components/DisplayMessages.jsx
+++ lib/components/DisplayMessages.jsx
@@ -24,31 +24,3 @@ export default class DisplayMessages extends Component {
     );
   }
 }
-
-// const DisplayMessages = ({messages, filteredMessages})=>{
-// //
-// //     if(filteredMessages > 0){
-// //       let messages = filteredMessages;
-// //     }else{
-// //       return messages;
-// //     }
-// //
-// //
-//     return(
-//       <ul className='message-field'>
-//         {messages.map(m =>{
-//           return (
-//             <li className='single-message' key={m.key}>
-//               <span className = 'timestamp'>{m.createdAt}</span>
-//               <span className = 'username'>{m.user.displayName}</span>
-//               <br/>
-//               <span className= 'message-content'>{m.content}</span>
-//             </li>
-//           );
-//         })
-//       }
-//       </ul>
-//     );
-//   }
-//
-// module.exports = DisplayMessages;
