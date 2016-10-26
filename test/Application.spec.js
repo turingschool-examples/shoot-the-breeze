54da2dbf4dcc2f7138fee503144c10c4f968a203
KINAN WHYTE <kinanwhyte@gmail.com>
2016-10-26 09:03:09 -0600

    splits tests into different files

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -4,7 +4,7 @@ import { shallow, mount, render } from 'enzyme'
 import { assert } from 'chai'
 
 import Application from '../lib/components/Application'
-import MessageInput from '../lib/components/MessageInput'
+
 
 describe('Unit Test | Application', () => {
   it('can mount with no properties', () => {
@@ -20,59 +20,6 @@ describe('Unit Test | Application', () => {
   //   assert.equal(wrapper.contains('turingschool')).to.be.true
   // })
 })
-
-describe('Unit Test | MessageInput', () => {
-  it('can mount with no properties', () => {
-    const wrapper = shallow(<MessageInput />)
-  })
-  // it('calls componentDidMount', () => {
-  //   sinon.spy(MessageInput.prototype, 'componentDidMount')
-  //   const wrapper = mount(<MessageInput />)
-  //   expect(MessageInput.prototype.componentDidMount.calledOnce).to.equal(true)
-  // })
-  it('renders a "submit-button"', () => {
-    const wrapper = mount(<MessageInput />)
-    assert(wrapper.find('.submit-button'))
-  })
-  it('renders a "clear-button"', () => {
-    const wrapper = shallow(<MessageInput />)
-    assert(wrapper.find('.clear-button'))
-  })
-  it('renders as a <form>', () => {
-    const wrapper = shallow(<MessageInput />)
-    assert.equal(wrapper.type(), 'form')
-  })
-// ________
-  // it('simulates click events', () => {
-  //   const onButtonClick = sinon.spy()
-  //   const wrapper = shallow(<MessageInput />)
-  // })
-  //
-  //
-})
-
-describe('Feature Test | MessageInput', () => {
-  it('should change the "draftMessage" state with new user input', () => {
-    const wrapper = mount(<MessageInput />)
-    const messageInput = wrapper.find('.message-input-form')
-
-    messageInput.simulate('change', {target: {value: 'how you doin?!'} })
-    setTimeout(() => {
-      assert.equal(wrapper.state('draftMessage'), ('how you doin?!'))
-    }, 1)
-  })
-  it('can render a message when "submit-button" is clicked"', () => {
-    const wrapper = mount(<Application />)
-    const messageInput = wrapper.find('.message-input-form')
-
-    messageInput.simulate('change', {target: {value: 'goodmorning'} })
-    wrapper.find('.submit-button').simulate('click')
-    setTimeout(() => {
-      assert.equal(wrapper.text(), ('goodmorning'))
-    }, 1)
-  })
-})
-
 
 
 // console.log(wrapper.debug())
